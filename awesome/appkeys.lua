3b72830bd9de46c19ab6a17040fbf7f0910c0523
Adnan Zafar <adnanjzafar@gmail.com>
2012-07-04 09:26:38 -0400

    Use Firefox as BROWSER

--- awesome/appkeys.lua
+++ awesome/appkeys.lua
@@ -1,5 +1,7 @@
-Entry { key = 'c'     , cmd = 'chrome'   }
-Entry { key = 'e'     , cmd = 'epdfview' }
-Entry { key = 'f'     , cmd = 'firefox'  }
-Entry { key = 'p'     , cmd = 'epdfview' }
-Entry { key = 'space' , cmd = 'chrome'   }
+-- Tab /[{},]/l1l1l0l1l1
+Entry { key = 'c'    , cmd = 'chrome'                }
+Entry { key = 'e'    , cmd = 'epdfview'              }
+Entry { key = 'z'    , cmd = 'zathura'               }
+Entry { key = 'f'    , cmd = 'firefox -p -no-remote' }
+Entry { key = 'p'    , cmd = 'epdfview'              }
+Entry { key = 'space', cmd = os.getenv('BROWSER')    }

a25d0d0c44321566dc7fdbd9454c2c5a2de072e3
Adnan Zafar <adnanjzafar@gmail.com>
2013-03-04 12:11:18 -0500

    awesome: Remove epdfview hotkey

--- awesome/appkeys.lua
+++ awesome/appkeys.lua
@@ -3,5 +3,4 @@ Entry { key = 'c'    , cmd = 'chrome'                }
 Entry { key = 'e'    , cmd = 'epdfview'              }
 Entry { key = 'z'    , cmd = 'zathura'               }
 Entry { key = 'f'    , cmd = 'firefox -p -no-remote' }
-Entry { key = 'p'    , cmd = 'epdfview'              }
 Entry { key = 'space', cmd = os.getenv('BROWSER')    }

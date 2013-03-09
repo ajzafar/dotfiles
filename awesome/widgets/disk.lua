dd4ef0af798343ba48e4e8aae2dd63c39e11df00
Adnan Zafar <adnanjzafar@gmail.com>
2013-03-09 15:39:32 -0500

    awesome: Add a menu to the disk widget
    The menu shows all mounts as well as specific usage values. Clicking on
    a mount switches to it in the widget.

--- awesome/widgets/disk.lua
+++ awesome/widgets/disk.lua
@@ -7,14 +7,31 @@ local function cycle_idx(w)
     end
 end
 
+local function create_menu(widget)
+    local entries = {}
+
+    for i,data in ipairs(widget.menu_data) do
+        table.insert(entries,
+                     { string.format("%s\t%s/%s",
+                                     widget.mounts[i], data.used, data.size),
+                       function()
+                           widget.idx = i - 1
+                           vicious.force{widget}
+                       end })
+    end
+
+    widget.menu = awful.menu({ items = entries })
+end
+
 function disk.new(args)
     local args = args or {}
     local widget = wibox.layout.flex.vertical()
 
-    widget.label = wibox.widget.textbox()
-    widget.bar = awful.widget.progressbar()
-    widget.mounts = args.mounts or { '/' }
-    widget.idx = 0
+    widget.label     = wibox.widget.textbox()
+    widget.bar       = awful.widget.progressbar()
+    widget.mounts    = args.mounts or { '/' }
+    widget.idx       = 0
+    widget.menu_data = {}
 
     -- label.width = 100
     widget.label:set_align('left')
@@ -27,6 +44,15 @@ function disk.new(args)
     for i,v in ipairs{widget.label, widget.bar} do
         local wid = v.widget or v
         wid:buttons(awful.button({}, 1, function() vicious.force{widget} end))
+        wid:buttons(awful.button({}, 3,
+                    function()
+                        if widget.menu and widget.menu.wibox.visible then
+                            widget.menu:hide()
+                        else
+                            create_menu(widget)
+                            widget.menu:show()
+                        end
+                    end))
     end
 
     return widget
@@ -41,6 +67,12 @@ function disk.vicious_format(widget, args)
 
     widget.bar:set_value(used / size)
     widget.label:set_text(string.format('%s %0.1f/%0.1f', mount, used, size))
+
+    for i,m in ipairs(widget.mounts) do
+        widget.menu_data[i] = {}
+        widget.menu_data[i].used = args['{' .. m .. ' used_gb}'] or 0
+        widget.menu_data[i].size = args['{' .. m .. ' size_gb}'] or 1
+    end
 end
 
 return disk

b2f4e926d266f49203e54e66eed629d38bba2969
Adnan Zafar <adnanjzafar@gmail.com>
2014-02-03 10:18:05 -0500

    Use conky with i3

---
+++ i3/conky/escape.lua
@@ -0,0 +1,8 @@
+-- Define a conky_escape function to aid in escaping any fields that may not
+-- play well with JSON.
+--
+-- So far that means escape double quotes
+
+function conky_escape(str)
+    return conky_parse(str):gsub('"', '\\"')
+end

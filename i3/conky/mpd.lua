3e882b8c20018ac0bc4642da79a8ab7ce5bf70f6
Adnan Zafar <adnanjzafar@gmail.com>
2014-03-28 23:58:39 -0400

    Escape things for pango

--- i3/conky/mpd.lua
+++ i3/conky/mpd.lua
@@ -2,15 +2,20 @@ file = file or ''
 
 function conky_cursong()
     local info = {}
+
     for i,v in ipairs{'artist', 'album', 'title', 'track', 'file'} do
         info[v] = conky_parse('$mpd_' .. v)
     end
 
     if info['file'] ~= file then
         file = info['file']
-        os.execute('notify-send ' .. string.format("'%s' '%s\n%s: %s'",
-                info['artist'], info['album'], info['track'], info['title']))
+        os.execute(pango_escape(string.format("notify-send '%s' '%s\n%s: %s'",
+                info['artist'], info['album'], info['track'], info['title'])))
     end
 
     return ''
+end
+
+function pango_escape(str)
+    return str:gsub('&', '&amp;')
 end

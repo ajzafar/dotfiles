5737288aa8b299bffc22e156ad094b05fb5abaa4
Adnan Zafar <adnanjzafar@gmail.com>
2022-01-03 22:08:11 -0500

    Remove awesome
    I've been using i3 for years, and although the occasional thought to go
    back to awesome pops into my head, it's never going to happen.

--- awesome/appkeys.lua
+++
@@ -1,6 +0,0 @@
--- Tab /[{},]/l1l1l0l1l1
-Entry { key = 'c'    , cmd = 'chrome'                }
-Entry { key = 'e'    , cmd = 'epdfview'              }
-Entry { key = 'z'    , cmd = 'zathura'               }
-Entry { key = 'f'    , cmd = 'firefox -p -no-remote' }
-Entry { key = 'space', cmd = os.getenv('BROWSER')    }
--- awesome/widgets/init.lua
+++
@@ -1,9 +0,0 @@
-widgets = {}
-
-widgets.battery = require('widgets.battery')
-widgets.cpu     = require('widgets.cpu')
-widgets.disk    = require('widgets.disk')
-widgets.memory  = require('widgets.memory')
-widgets.mpd     = require('widgets.mpd')
-
-return widgets
